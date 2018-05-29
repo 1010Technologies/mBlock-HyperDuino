85e3dc458d3544eebbc133e83febdcbdca03066b
chris-torrence <chris_torrence@comcast.net>
2018-05-28 21:21:57 -0600

    Fix motor blocks to work with different HD boards, add speed A/B

--- src/HyperDuino.cpp
+++ src/HyperDuino.cpp
@@ -15,90 +15,99 @@ bool isPinTouched(int pin)
   return false;
 }
 
-// Do we have support for motors?
-#ifdef MOTOR_A1
+byte MOTOR_A1 = 0;
+byte MOTOR_A2 = 0;
+byte MOTOR_ASPEED = 0;
+byte MOTOR_B1 = 0;
+byte MOTOR_B2 = 0;
+byte MOTOR_BSPEED = 0;
+byte MOTOR_ENABLE = 0;
 
-void hd_MotorSetup()
+// HyperDuino board versions
+// 100 = 1.0 (no motor pins)
+// 240 = 2.4
+// 350 = 3.5
+// 400 = 4.0
+void hd_MotorSetup(int board)
 {
-  pinMode(MOTOR_B1,OUTPUT);
-  pinMode(MOTOR_B2,OUTPUT);
-  pinMode(MOTOR_A1,OUTPUT);
-  pinMode(MOTOR_A2,OUTPUT);
+  if (board == 400)
+  {
+    MOTOR_A1 = 3;
+    MOTOR_A2 = 4;
+    MOTOR_ASPEED = 5;
+    MOTOR_B1 = 7;
+    MOTOR_B2 = 8;
+    MOTOR_BSPEED = 6;
+  }
+  else if (board == 350)
+  {
+    MOTOR_A1 = 8;
+    MOTOR_A2 = 9;
+    MOTOR_ASPEED = 10;
+    MOTOR_B1 = 12;
+    MOTOR_B2 = 13;
+    MOTOR_BSPEED = 11;
+  }
+  else if (board == 240)
+  {
+    MOTOR_ENABLE = 6;
+    MOTOR_A1 = 7;
+    MOTOR_A2 = 8;
+    MOTOR_ASPEED = 9;
+    MOTOR_B1 = 4;
+    MOTOR_B2 = 5;
+    MOTOR_BSPEED = 3;
+  }
 }
 
-void hd_MotorForward(int speed)
+void hd_MotorMove(int speedA, int speedB)
 {
-#ifdef MOTOR_ENABLE
-  digitalWrite(MOTOR_ENABLE,HIGH);
-#endif
-  analogWrite(MOTOR_ASPEED,speed);
-  analogWrite(MOTOR_BSPEED,speed);
-  digitalWrite(MOTOR_A1,HIGH);
-  digitalWrite(MOTOR_A2,LOW);
-  digitalWrite(MOTOR_B1,HIGH);
-  digitalWrite(MOTOR_B2,LOW);
+  if (MOTOR_A1 != 0)
+  {
+    pinMode(MOTOR_B1,OUTPUT);
+    pinMode(MOTOR_B2,OUTPUT);
+    pinMode(MOTOR_A1,OUTPUT);
+    pinMode(MOTOR_A2,OUTPUT);
+    if (MOTOR_ENABLE != 0) digitalWrite(MOTOR_ENABLE,HIGH);
+    analogWrite(MOTOR_ASPEED,abs(speedA));
+    analogWrite(MOTOR_BSPEED,abs(speedB));
+    digitalWrite(MOTOR_A1, speedA > 0 ? HIGH : LOW);
+    digitalWrite(MOTOR_A2, speedA > 0 ? LOW : HIGH);
+    digitalWrite(MOTOR_B1, speedB > 0 ? HIGH : LOW);
+    digitalWrite(MOTOR_B2, speedB > 0 ? LOW : HIGH);
+  }
 }
 
-void hd_MotorBackward(int speed)
+void hd_MotorForward(int speedA, int speedB)
 {
-#ifdef MOTOR_ENABLE
-  digitalWrite(MOTOR_ENABLE,HIGH);
-#endif
-  analogWrite(MOTOR_ASPEED,speed);
-  analogWrite(MOTOR_BSPEED,speed);
-  digitalWrite(MOTOR_A1,LOW);
-  digitalWrite(MOTOR_A2,HIGH);
-  digitalWrite(MOTOR_B1,LOW);
-  digitalWrite(MOTOR_B2,HIGH);
+  hd_MotorMove(speedA, speedB);
+}
+
+void hd_MotorBackward(int speedA, int speedB)
+{
+  hd_MotorMove(-speedA, -speedB);
 }
 
 void hd_MotorLeft(int speed)
 {
-#ifdef MOTOR_ENABLE
-  digitalWrite(MOTOR_ENABLE,HIGH);
-#endif
-  analogWrite(MOTOR_ASPEED,speed);
-  analogWrite(MOTOR_BSPEED,speed);
-  digitalWrite(MOTOR_A1,HIGH);
-  digitalWrite(MOTOR_A2,LOW);
-  digitalWrite(MOTOR_B1,LOW);
-  digitalWrite(MOTOR_B2,HIGH);
+  hd_MotorMove(abs(speed), -abs(speed));
 }
 
 void hd_MotorRight(int speed)
 {
-#ifdef MOTOR_ENABLE
-  digitalWrite(MOTOR_ENABLE,HIGH);
-#endif
-  analogWrite(MOTOR_ASPEED,speed);
-  analogWrite(MOTOR_BSPEED,speed);
-  digitalWrite(MOTOR_A1,LOW);
-  digitalWrite(MOTOR_A2,HIGH);
-  digitalWrite(MOTOR_B1,HIGH);
-  digitalWrite(MOTOR_B2,LOW);
+  hd_MotorMove(-abs(speed), abs(speed));
 }
 
 void hd_MotorStop(void)
 {
-#ifdef MOTOR_ENABLE
-  digitalWrite(MOTOR_ENABLE,LOW);
-#endif
-  analogWrite(MOTOR_BSPEED,0);
-  analogWrite(MOTOR_ASPEED,0);
-  digitalWrite(MOTOR_A1,LOW);
-  digitalWrite(MOTOR_A2,LOW);
-  digitalWrite(MOTOR_B1,LOW);
-  digitalWrite(MOTOR_B2,LOW);
+  if (MOTOR_A1 != 0)
+  {
+    if (MOTOR_ENABLE != 0) digitalWrite(MOTOR_ENABLE, LOW);
+    analogWrite(MOTOR_BSPEED, 0);
+    analogWrite(MOTOR_ASPEED, 0);
+    digitalWrite(MOTOR_A1, LOW);
+    digitalWrite(MOTOR_A2, LOW);
+    digitalWrite(MOTOR_B1, LOW);
+    digitalWrite(MOTOR_B2, LOW);
+  }
 }
-
-#else
-
-// No motor pins (HyperDuino board 1.0)
-void hd_MotorSetup() {}
-void hd_MotorForward(int speed) {}
-void hd_MotorBackward(int speed) {}
-void hd_MotorLeft(int speed) {}
-void hd_MotorRight(int speed) {}
-void hd_MotorStop(void) {}
-
-#endif
--- src/HyperDuino.h
+++ src/HyperDuino.h
@@ -3,43 +3,9 @@
 
 bool isPinTouched(int pin);
 
-// HyperDuino board versions
-// 100 = 1.0 (no motor pins)
-// 240 = 2.4
-// 350 = 3.5
-// 400 = 4.0
-#ifndef BOARD
-#define BOARD 400
-#endif
-
-#if BOARD == 400
-  #define MOTOR_A1 3
-  #define MOTOR_A2 4
-  #define MOTOR_ASPEED 5
-  #define MOTOR_B1 7
-  #define MOTOR_B2 8
-  #define MOTOR_BSPEED 6
-#elif BOARD == 350
-  #define MOTOR_A1 8
-  #define MOTOR_A2 9
-  #define MOTOR_ASPEED 10
-  #define MOTOR_B1 12
-  #define MOTOR_B2 13
-  #define MOTOR_BSPEED 11
-#elif BOARD == 240
-  #define MOTOR_ENABLE 6
-  #define MOTOR_A1 7
-  #define MOTOR_A2 8
-  #define MOTOR_ASPEED 9
-  #define MOTOR_B1 4
-  #define MOTOR_B2 5
-  #define MOTOR_BSPEED 3
-#else
-#endif
-
-void hd_MotorSetup();
-void hd_MotorForward(int speed);
-void hd_MotorBackward(int speed);
+void hd_MotorSetup(int board);
+void hd_MotorForward(int speedA, int speedB);
+void hd_MotorBackward(int speedA, int speedB);
 void hd_MotorLeft(int speed);
 void hd_MotorRight(int speed);
 void hd_MotorStop(void);
